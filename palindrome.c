1a42167211b97b6f1e0dea1999f36d0f51265dcf
Lnatic96 <zeljko.vorp@gmail.com>
2017-06-22 10:19:15 +0200

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    int n, reversedInteger = 0, remainder, originalInteger;
+    int n, rec = 0, remainder, originalInteger;
 
     printf("Uneti Broj: ");
     scanf("%d", &n);
 
     originalInteger = n;
-
-    // reversed integer is stored in variable
     while( n!=0 )
     {
         remainder = n%10;
-        reversedInteger = reversedInteger*10 + remainder;
+        rec = rec*10 + remainder;
         n /= 10;
     }
 
-    // palindrome if orignalInteger and reversedInteger are equal
-    if (originalInteger == reversedInteger)
+    if (originalInteger == rec)
         printf("%d je palindrom.", originalInteger);
     else
         printf("%d nije palindrom.", originalInteger);
